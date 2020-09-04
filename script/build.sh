0f661df814fbf003b39d7d3a41f5304032f84ada
林松雄 <linsongxiong@qudian.com>
2020-09-04 13:12:56 +0800

    [UPD]脚本

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 docker rmi oss-server:dev
-docker build . -t oss-server:dev
+docker build .. -t oss-server:dev
 docker run -d --name oss-server -v /Users/qudian/Documents/oss-server-volum:/oss -p 18080:18080 oss-server:dev
